1e7962566c40e5a76bfba00b470b300baf49432d
Vitaliy Derda <derdavitaliy@gmail.com>
2021-02-28 18:29:52 +0200

    fix: update auth api and fix create current week

--- helpers/checkCurrentWeek.js
+++ helpers/checkCurrentWeek.js
@@ -1,10 +1,15 @@
 //Packages
-const dayjs = require('dayjs');
+const { DateTime } = require('luxon');
 //Models
 const weekModel = require('../models/week');
 
+/**
+ * This function checks the current week.
+ * If the current week is over, then it returns false.
+ * If the current week isn't over, it returns the current week.
+ */
 async function checkCurrentWeek(currentWeekId) {
-	const startWeek = dayjs().startOf('week').add(1, 'day').format('DD-MM-YYYY');
+	const startWeek = DateTime.local().startOf('week').toFormat('dd-MM-yyyy');
 	const userCurrentWeek = await weekModel.findOne({ _id: currentWeekId }).populate('tasks');
 
 	return userCurrentWeek.startWeekDate === startWeek ? userCurrentWeek : false;
--- helpers/createCurrentWeek.js
+++ helpers/createCurrentWeek.js
@@ -1,5 +1,5 @@
 //Packages
-const dayjs = require('dayjs');
+const { DateTime } = require('luxon');
 //Models
 const taskModel = require('../models/task');
 const weekModel = require('../models/week');
@@ -8,11 +8,15 @@ const createDaysOfWeek = require('./createDaysOfWeek');
 //Data
 const initialTasks = require('../data/initialTasks');
 
+/**
+ * This function returns the current week with start and end date.
+ * It also returns all default tasks for the current week.
+ */
 async function createCurrentWeek() {
 	let tasksForWeek = [];
 	const defaultTasks = [...initialTasks];
 
-	const startWeek = dayjs().startOf('week').add(1, 'day');
+	const startWeek = DateTime.local().startOf('week');
 	const daysOfWeek = createDaysOfWeek();
 
 	for (let i = 0; i < defaultTasks.length; i++) {
@@ -27,8 +31,8 @@ async function createCurrentWeek() {
 	}
 
 	const currentWeek = await weekModel.create({
-		startWeekDate: startWeek.format('DD-MM-YYYY'),
-		endWeekDate: startWeek.add(6, 'day').format('DD-MM-YYYY'),
+		startWeekDate: startWeek.toFormat('dd-MM-yyyy'),
+		endWeekDate: startWeek.plus({ days: 6 }).toFormat('dd-MM-yyyy'),
 		pointsGained: 0,
 		pointsPlanned: 0,
 		tasks: tasksForWeek,
